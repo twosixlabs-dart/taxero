5652072e51b22725214cccdd740345b88eb2ce07
Maria <maxaalexeeva@gmail.com>
2020-11-12 23:05:57 -0700

    working index cdr; need cleanup; new fields in application

--- src/main/scala/org/clulab/taxero/IndexCDRs.scala
+++ src/main/scala/org/clulab/taxero/IndexCDRs.scala
@@ -1,7 +1,6 @@
-package ai.lum.eidosdottir.apps
+package ai.lum.taxero
 
 import java.io.File
-import java.nio.file.{Files, Path, Paths}
 
 import com.typesafe.scalalogging.LazyLogging
 import ai.lum.common.ConfigFactory
@@ -12,52 +11,39 @@ import ai.lum.odinson.extra.ProcessorsUtils
 import ai.lum.odinson.extra.IndexDocuments
 import org.clulab.processors.fastnlp.FastNLPProcessor
 
-object IndexCDRs extends App with LazyLogging {
+import scala.io.Source
+
+object IndexCDRs extends App {
 
   val config = ConfigFactory.load()
   val proc = new FastNLPProcessor()
   val writer = OdinsonIndexWriter.fromConfig()
-
-  // dir of cdrs
   val cdrDir = config.apply[File]("apps.cdrDir")
 
   val dataDir = config.apply[String]("odinson.dataDir")
   val textDir = new File(s"$dataDir/text")
   val docsDir = new File(s"$dataDir/docs")
 
-  logger.info("Annotating documents...")
-  cdrDir.listFilesByWildcard("*.json").par foreach { cdr =>
-    val basename = cdr.getBaseName()
-    try {
-      val docFilename = s"$docsDir/$basename.json"
-      if (!Files.exists(Paths.get(docFilename))) {
-        val json = ujson.read(cdr)
-        val text = json("extracted_text").str
-//        println(text + "<<")
-        // Annotate with Processors
-        val procDoc = proc.annotate(text)
-        // make the OdinsonDoc
-        val doc = ProcessorsUtils.convertDocument(procDoc)
-        // save text
-        val textFile = new File(s"$textDir/$basename.txt")
-        textFile.writeString(text)
-        // save doc
-        val docFile = new File(docFilename)
-        docFile.writeString(doc.toJson)
-      } else {
-        logger.error(s"skipping $basename because $docFilename already exists!")
-      }
-    } catch {
-      case e: StringIndexOutOfBoundsException => logger.error(s"File $basename wasn't processed.")
-    }
+  val cdrFiles = cdrDir.listFiles()
+
+  for (f <- cdrFiles) {
+    val basename = f.getBaseName()
+    val json = ujson.read(f)
+    val text = json("extracted_text").str
+    val procDoc = proc.annotate(text)
+    // make the OdinsonDoc
+    val doc = ProcessorsUtils.convertDocument(procDoc)
+    // save text
+    val textFile = new File(s"$textDir/$basename.txt")
+    textFile.writeString(text)
+      // save doc
+     val docFilename = s"$docsDir/$basename.json"
+    val docFile = new File(docFilename)
+    docFile.writeString(doc.toJson)
   }
-
-  val docFiles = docsDir
-    .listFilesByWildcards(Seq("*.json", "*.json.gz"), recursive = true)
-    .par
-
-  logger.info("Indexing documents")
+  val docFiles = docsDir.listFiles()
   IndexDocuments.indexDocuments(writer, docFiles)
   writer.close
+
 
 }
